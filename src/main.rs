78b03823dd4457aff13c3622551c17fe9a38833c
ar7eniyan <ar7eniy.k@gmail.com>
2024-09-02 22:04:42 +0300

    start implementing ethernet communication in rust

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,69 @@
-use pnet::datalink::NetworkInterface;
+use std::os::fd::AsRawFd;
+
+use nix::{
+    libc::{sockaddr_ll, AF_PACKET},
+    net::if_::if_nametoindex,
+    sys::socket::{
+        bind, recvfrom, sendto, sockaddr, socket, AddressFamily, LinkAddr, SockFlag, SockProtocol,
+        SockType, SockaddrLike,
+    },
+};
 
 const IFNAME: &str = "enp4s0";
 
-fn main() -> Result<(), String> {
-    let interfaces: Vec<NetworkInterface> = pnet::datalink::interfaces();
-    dbg!(&interfaces);
+fn perror_fmt(message: impl AsRef<str>) -> impl Fn(nix::errno::Errno) -> String {
+    move |errno| message.as_ref().to_string() + ": " + errno.desc()
+}
 
-    let eth_if: &NetworkInterface = interfaces
-        .iter()
-        .find(|&interface| interface.name == IFNAME)
-        .ok_or_else(|| format!("No interface named {}", IFNAME))?;
-    dbg!(&eth_if);
+#[derive(Debug)]
+struct EthernetComms {
+    sockfd: std::os::fd::OwnedFd,
+    peer_address: LinkAddr,
+}
+
+impl EthernetComms {
+    fn new(ethertype: u16, ifname: &str, peer_mac: [u8; 6]) -> Result<Self, String> {
+        let ifindex = if_nametoindex(IFNAME).map_err(perror_fmt(
+            "Can't get the index of the interface".to_owned() + ifname,
+        ))?;
+        println!("Using interface {ifname} (index {ifindex})");
+
+        let sockfd = socket(
+            AddressFamily::Packet,
+            SockType::Datagram,
+            SockFlag::empty(),
+            SockProtocol::EthAll, // Do I need to call htons()?
+        )
+        .map_err(perror_fmt("Can't create link layer socket"))?;
+
+        let mut peer_address = sockaddr_ll {
+            sll_family: AF_PACKET.try_into().unwrap(),
+            sll_protocol: ethertype,
+            sll_ifindex: ifindex.try_into().unwrap(),
+            sll_hatype: 0,
+            sll_pkttype: 0,
+            sll_halen: 6,
+            sll_addr: [0; 8],
+        };
+        peer_address.sll_addr[..6].clone_from_slice(&peer_mac);
+        let peer_address = unsafe {
+            LinkAddr::from_raw(&peer_address as *const sockaddr_ll as *const sockaddr, None)
+                .unwrap()
+        };
+
+        bind(sockfd.as_raw_fd(), &peer_address)
+            .map_err(perror_fmt("Can't bind to the interface and the protocol"))?;
+
+        Ok(Self {
+            sockfd,
+            peer_address,
+        })
+    }
+}
+
+fn main() -> Result<(), String> {
+    let comms = EthernetComms::new(0xDEAD, "enp4s0", [0; 6])?;
+    dbg!(comms);
 
     Ok(())
 }
